1650d628024fb34c0d98d21cdbbdfe92f666e1d0
gopher <lovesunshine269@gmail.com>
2021-12-05 11:21:29 +0700

    add routes

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {createUser, getUsers, getUserByID, deleteUser, updateUser} from '../controllers/user.js';
+
+
+const router = express.Router();
+
+
+router.get('/', getUsers);
+
+router.post('/', createUser);
+
+router.get('/:id', getUserByID);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
+export default router;
